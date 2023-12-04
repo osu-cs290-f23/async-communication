897f8ee962b8a5d2aad97995919f6709eaff39ff
Rob Hess <hessro@oregonstate.edu>
2023-12-04 15:32:55 -0800

    Add code from 12/4 lecture.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require("fs")
 var express = require('express')
 var exphbs = require('express-handlebars')
 
@@ -9,6 +10,7 @@ var port = process.env.PORT || 8000
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.json())
 app.use(express.static('static'))
 
 app.get('/', function (req, res, next) {
@@ -25,6 +27,37 @@ app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase()
   if (peopleData[person]) {
     res.status(200).render('photoPage', peopleData[person])
+  } else {
+    next()
+  }
+})
+
+app.post('/people/:person/addPhoto', function (req, res, next) {
+  console.log("  -- req.body:", req.body)
+  var person = req.params.person.toLowerCase()
+  if (peopleData[person]) {
+    if (req.body && req.body.url && req.body.caption) {
+      peopleData[person].photos.push({
+        caption: req.body.caption,
+        url: req.body.url
+      })
+
+      fs.writeFile(
+        "./peopleData.json",
+        JSON.stringify(peopleData, null, 2),
+        function (err) {
+          if (err) {
+            res.status(500).send("Error writing photo to DB")
+          } else {
+            res.status(200).send("Photo successfully added to DB")
+          }
+        }
+      )
+    } else {
+      res.status(400).send(
+        "Requests need a JSON body with 'url' and 'caption'"
+      )
+    }
   } else {
     next()
   }
